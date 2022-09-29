0554c1e5e49c72ed955ba1fe70257bb2401488e0
sea-grass <dev.seagrass@gmail.com>
2022-09-29 13:02:30 -0400

    Update to zig 0.10

--- src/main.zig
+++ src/main.zig
@@ -10,44 +10,45 @@ const Rook = @import("chess/rook.zig");
 const Cli = @import("cli.zig");
 
 pub fn main() anyerror!void {
-  const allocator = std.testing.allocator;
-  var args_it = try std.process.argsWithAllocator(allocator);
-  defer args_it.deinit();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    var args_it = try std.process.argsWithAllocator(allocator);
+    defer args_it.deinit();
 
-  // skip the executable name
-  _ = args_it.skip();
+    // skip the executable name
+    _ = args_it.skip();
 
-  var ran_command = false;
+    var ran_command = false;
 
-  if (args_it.next(allocator)) |arg| {
-    if (std.mem.eql(u8, "cli", try arg)) {
-      try Cli.run();
-      ran_command = true;
+    if (args_it.next()) |arg| {
+        if (std.mem.eql(u8, "cli", arg)) {
+            try Cli.run();
+            ran_command = true;
+        }
     }
-  }
 
-  if (!ran_command) {
-    try testing();
-  }
-
+    if (!ran_command) {
+        try testing();
+    }
 }
 
 fn testing() !void {
-  const bb = 1 << 13;
+    const bb = 1 << 13;
 
-  try stdout.print("origin\n", .{});
-  try printBitboard(bb, stdout);
+    try stdout.print("origin\n", .{});
+    try printBitboard(bb, stdout);
 
-  // const king_moves = King.moves(bb);
-  // try stdout.print("king moves\n", .{});
-  // try printBitboard(king_moves, stdout);
+    // const king_moves = King.moves(bb);
+    // try stdout.print("king moves\n", .{});
+    // try printBitboard(king_moves, stdout);
 
-  try stdout.print("knight moves\n", .{});
-  try printBitboard(Knight.moves(bb), stdout);
+    try stdout.print("knight moves\n", .{});
+    try printBitboard(Knight.moves(bb), stdout);
 
-  try stdout.print("Chess board\n", .{});
-  try Board.printBoard(stdout);
+    try stdout.print("Chess board\n", .{});
+    try Board.printBoard(stdout);
 
-  try stdout.print("Rook moves\n", .{});
-  try printBitboard(Rook.moves(bb), stdout);
+    try stdout.print("Rook moves\n", .{});
+    try printBitboard(Rook.moves(bb), stdout);
 }
