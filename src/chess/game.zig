405a6c7ed80ecce0211cc2ee5a858228ba5a277a
sea-grass <dev.seagrass@gmail.com>
2022-12-02 14:28:48 -0500

    Track game moves

--- src/chess/game.zig
+++ src/chess/game.zig
@@ -2,7 +2,33 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const Board = @import("board.zig").Board;
-const Move = struct {};
+
+pub const Move = struct {
+    piece: Piece = undefined,
+    from: [2:0]u8 = undefined,
+    to: [2:0]u8 = undefined,
+};
+
+const PieceType = enum {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+};
+
+const Side = enum {
+    Black,
+    White,
+};
+
+const Piece = struct {
+    side: Side,
+    piece: PieceType,
+};
+
+const p = Piece{.side = .Black, .piece = .King};
 
 pub const Game = struct{
     board: Board,
@@ -21,5 +47,10 @@ pub const Game = struct{
 
     pub fn deinit(self: *Game) void {
         self.moves.deinit();
+    }
+
+    pub fn move(self: *Game, m: Move) !void {
+        const m2 = Move { .piece = Piece{.side=.Black,.piece=.Pawn}, .from=m.from, .to=m.to};
+        try self.moves.append(m2);
     }
 };
--- src/commands/game.zig
+++ src/commands/game.zig
@@ -1,11 +1,40 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const Game = @import("../chess/game.zig").Game;
+const Move = @import("../chess/game.zig").Move;
 
 pub fn run(_: Allocator, _: [][]const u8) !void {
+    var buffer: [64:0]u8 = undefined;
     const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
     var game = Game.init(std.heap.page_allocator);
     defer game.deinit();
+
+    try game.board.printBoard(stdout);
+    try stdout.print("Turn {d}\n", .{ game.turn });
+    try stdout.print("Num moves {d}\n", .{game.moves.items.len});
+
+    var move = Move{};
+    
+    {
+        try stdout.print("from: ", .{});
+        var line = (try stdin.readUntilDelimiterOrEof(&buffer, '\n')).?;
+        if (line.len == 2) {
+            move.from[0] = line[0];
+            move.from[1] = line[1];
+        }
+    }
+
+    {
+        try stdout.print("to: ", .{});
+        var line = (try stdin.readUntilDelimiterOrEof(&buffer, '\n')).?;
+        if (line.len == 2) {
+            move.to[0] = line[0];
+            move.to[1] = line[1];
+        }
+    }
+
+    try game.move(move);
 
     try game.board.printBoard(stdout);
     try stdout.print("Turn {d}\n", .{ game.turn });
